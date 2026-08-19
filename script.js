0eb2225892f68e746421b3085058f43934a57b87
Saheb Chhabra <sahibchhabraprog@gmail.com>
2026-08-19 19:46:01 +0530

    made navbar responsive

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ form.addEventListener('submit' , (e)=>{
     alert('Form submitted successfully!')
     e.preventDefault();
 })
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+const menuIcon = menuBtn.querySelector("i");
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+    } else {
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+    }
+});
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+    });
+});
